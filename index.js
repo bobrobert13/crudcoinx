7d2f540a07ca169efd8e356cb8d6a580bf70cc2c
Robert Caraballo <rob132ca@gmail.com>
2022-04-27 22:50:59 -0400

    cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require('body-parser');
 const route = require("./net/route");
-
+const cors = require("cors")
 const app = express();
 
 
